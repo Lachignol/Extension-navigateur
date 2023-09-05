34a38d9a7b405c6d822cac771e4de465a8753501
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-09-05 16:55:14 +0200

    v2

--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,64 @@
-const body = document.querySelector("body");
 
-//Methode pour chat gpt 
-if (body) {
-  const text = body.textContent;
+const text = document.querySelectorAll(
+  "h1,h2,h3,h4,h5,h6,p,li,td,caption,span,a"
+);
+async function getJson()
+{
+    let url = "http://127.0.0.1:5500/scripts/traduction-jeunes.json"
+    try
+    {
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json)
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] in json) {
+              text[i].innerHTML = text[i].innerHTML.replace(`${text[i]}`,`${Json.mots}`);
+            } else if (text[i].innerHTML.includes("Brady")) {
+              text[i].innerHTML = text[i].innerHTML.replace("Brady","6-time Super Bowl champion Tom Brady");
+            }
+          }
+        
+    }
+    
+    catch(erreur)
+    {
+        console.log("Erreur : ", erreur.message);
+    }
+}
+
+getJson()
+
+/*
+for (let i = 0; i < text.length; i++) {
+  if (text[i] in json) {
+    text[i].innerHTML = text[i].innerHTML.replace(`${text[i]}`,`${Json.mots}`);
+  } else if (text[i].innerHTML.includes("Brady")) {
+    text[i].innerHTML = text[i].innerHTML.replace("Brady","6-time Super Bowl champion Tom Brady");
+  }
+}
+
+/*
+function lookupDefinition(word) {
+    const dictionary = {
+      "fête": {
+        "mot": "AFTER",
+        "definition": "Désigne une fête qui se donne suite à la fête de la nuit et se prolonge le matin"
+      },
+      // ... (other entries)
+    };
+    if (word in dictionary) {
+      return dictionary[word].definition;
+    } else {
+      return "Word not found in the dictionary.";
+    }
+  }
+  const wordToLookup = "fête";
+  const definition = lookupDefinition(wordToLookup);
+  console.log(definition);
+
+
   const resultTranslategpt = getTranslate(text);
-  const correspondanceJson = /[^\s]+/g; // definir comment chercher les mots correspondant au json
+  const correspondanceJson = getCorrespondance(resultTranslategpt) // definir comment chercher les mots correspondant au json
   const resultFinal = text.matchAll(correspondanceJson);
   const badge = document.createElement("p");
   // Use the same styling as the publish information in an article's header
@@ -33,3 +87,12 @@ async function getTranslate(text){
 }
 
 
+function getCorrespondance(resultTranslategpt){
+let splitResultTranslategpt = resultTranslategpt.split(" ")
+    for(let i=0;i<splitResultTranslategpt.length;i++)
+    replace(splitResultTranslategpt[i], traduction-jeunes.json.resultTranslategpt[i].mots);
+
+})
+
+
+*/
