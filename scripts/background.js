28e459501e93504656dc6ec09c35f35625bb14bc
oscorchan <oscar.cortijos@icloud.com>
2023-09-11 15:48:35 +0200

    Ajout du bouton On/Off pour lire la musique

---
+++ scripts/background.js
@@ -0,0 +1,42 @@
+function playAudioCode() {
+    // ce code sera exécuté directement dans la page de contenu
+    const audioElement = new Audio(chrome.runtime.getURL('musics/GTA.mp3'));
+    audioElement.play();
+    window.__audioElement = audioElement;  // stocker dans une variable globale pour un accès ultérieur
+}
+
+function stopAudioCode() {
+    // ce code arrête la musique sur la page de contenu
+    if (window.__audioElement) {
+        window.__audioElement.pause();
+        window.__audioElement.currentTime = 0;
+    }
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setBadgeText({
+        text: "OFF",
+    });
+});
+
+chrome.action.onClicked.addListener(async (tab) => {
+    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+
+    await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: nextState,
+    });
+
+    if (nextState === "ON") {
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: playAudioCode
+        });
+    } else if (nextState === "OFF") {
+        await chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: stopAudioCode
+        });
+    }
+});
