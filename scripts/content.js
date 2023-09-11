3e1af68045b3c99afc4e0f5da24cd4d3c1f8650d
oscorchan <oscar.cortijos@icloud.com>
2023-09-11 11:32:28 +0200

    optimisation

--- scripts/content.js
+++ scripts/content.js
@@ -1,131 +1,28 @@
-// let text = document.body
-// console.log(text)
-
-// const parcoursDom = (text, data) => {
-//   for (let node of text.childNodes) {
-//     if (node.hasChildNodes()) {
-//       parcoursDom(node, data)
-//     }
-//     else {
-//       console.log(node.textContent)
-//       for (let y = 0; y < Object.keys(data).length; y++) {
-//         let motRecherche = Object.keys(data)[y];
-//         let motRechercheMaj = ((Object.keys(data)[y]).charAt(0).toUpperCase()) + (Object.keys(data)[y]).slice(1);
-//         let motRechercheToutMaj = (Object.keys(data)[y]).toUpperCase()
-//         let motRemplace = data[motRecherche].mot;
-//         let motDefinition = data[motRecherche].definition
-//         if ((node.textContent.includes(motRecherche))) {
-//           node.innerHTML = node.innerHTML.replaceAll(motRecherche, `<span style="color: red" title="${motDefinition}">${motRemplace}</span>`);
-//         } else if ((node.textContent.includes(motRechercheMaj))) {
-//           node.innerHTML = node.innerHTML.replaceAll(motRechercheMaj, `<span style="color: red" title="${motDefinition}">${motRemplace}</span>`)
-//         } else if ((node.textContent.includes(motRechercheToutMaj))) {
-//           node.innerHTML = node.innerHTML.replaceAll(motRechercheToutMaj, `<span style="color: red" title="${motDefinition}">${motRemplace}</span>`)
-//         }
-//       }
-//     }
-//   }
-// }
-
-
-
-// async function getJson() {
-//   let url = "http://127.0.0.1:5500/scripts/traduction-jeunes.json"
-//   let fetcher = await fetch(url);
-//   let data = await fetcher.json();
-//   parcoursDom(text, data)
-// }
-
-
 async function getJson() {
-  let text = document.querySelectorAll('h1,h2,h3,h4,h5,h6,p,li,td,caption');
-  // .h:not(.img),.p:not(.img),.li:not(.img),.td:not(.img),.table:not(.img),.caption:not(.img),.a:not(.img),.span:not(.img)
-  let url = "http://127.0.0.1:5500/scripts/traduction-jeunes.json"
-  let fetcher = await fetch(url);
-  let data = await fetcher.json();
-  for (let i = 0; i < text.length; i++) {
-    for (let y = 0; y < Object.keys(data).length; y++) {
-      let motRecherche = Object.keys(data)[y];
-      let motRechercheMaj = ((Object.keys(data)[y]).charAt(0).toUpperCase()) + (Object.keys(data)[y]).slice(1);
-      let motRechercheToutMaj = (Object.keys(data)[y]).toUpperCase()
-      let motRemplace = data[motRecherche].mot;
-      let motDefinition = data[motRecherche].definition
-      console.log(motDefinition)
-      if ((text[i].innerHTML.includes(motRecherche)) && !(text[i].innerHTML.includes('img'))) {
-        text[i].innerHTML = text[i].innerHTML.replaceAll(motRecherche, `<span style="color: red" title="${motDefinition}">${motRemplace}</span>`);
-      } else if ((text[i].innerHTML.includes(motRechercheMaj)) && !(text[i].innerHTML.includes('img'))) {
-        text[i].innerHTML = text[i].innerHTML.replaceAll(motRechercheMaj, `<span style="color: red" title="${motDefinition}">${motRemplace}</span>`)
-      } else if ((text[i].innerHTML.includes(motRechercheToutMaj)) && !(text[i].innerHTML.includes('img'))) {
-        text[i].innerHTML = text[i].innerHTML.replaceAll(motRechercheToutMaj, `<span style="color: red" title="${motDefinition}">${motRemplace}</span>`)
-      }
-    }
+  try {
+      const url = "http://127.0.0.1:5500/scripts/traduction-jeunes.json";
+      const response = await fetch(url);
+      const data = await response.json();
+
+      const elements = document.querySelectorAll('h1,h2,h3,h4,h5,h6,p,td,caption');
+
+      elements.forEach(el => {
+          if (!el.querySelector('img')) {
+              Object.keys(data).forEach(key => {
+                // La méthode Object.keys() renvoie un tableau contenant les noms des propriétés propres à un objet
+                // .forEach permet de boucler sur tous les mots du dictionnaire (toutes les clefs)  
+                const regex = new RegExp(`\\b${key}\\b`, "ig");
+                // \b = début/fin de mot
+                // i = case-insensitive (so, for example, /a/i will match the string "a" or "A".
+                // g = global, match all instances of the pattern in a string, not just one
+                  const replacement = `<span style="color: red" title="${data[key].definition}">${data[key].mot}</span>`;
+                  el.innerHTML = el.innerHTML.replace(regex, replacement);
+              });
+          }
+      });
+  } catch (err) {
+      console.error("Error fetching or processing data:", err);
   }
 }
 
-getJson()
-
-
-//console.log(`${text[i].innerHTML = text[i].innerHTML.replace(text[i],jsonValue[text[i]].mot)}`);
-// //   }else if (text[i].innerHTML.includes("Brady")) {
-// //     text[i].innerHTML = text[i].innerHTML.replace("Brady","6-time Super Bowl champion Tom Brady");
-
-
-
-/*
-function lookupDefinition(word) {
-    const dictionary = {
-      "fête": {
-        "mot": "AFTER",
-        "definition": "Désigne une fête qui se donne suite à la fête de la nuit et se prolonge le matin"
-      },
-      // ... (other entries)
-    };
-    if (word in dictionary) {
-      return dictionary[word].definition;
-    } else {
-      return "Word not found in the dictionary.";
-    }
-  }
-  const wordToLookup = "fête";
-  const definition = lookupDefinition(wordToLookup);
-  console.log(definition);
-
-
-  const resultTranslategpt = getTranslate(text);
-  const correspondanceJson = getCorrespondance(resultTranslategpt) // definir comment chercher les mots correspondant au json
-  const resultFinal = text.matchAll(correspondanceJson);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `${resultFinal}`;
-
-
-  // defintion de la mise en page a voir 
-  
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-}
-
-
-//fonction de récupération de la trad faite par chatgpt
-
-async function getTranslate(text){
-
-    //definir la requete a envoyé a chatgpt avec la variable ${text} a rentré dans le prompt:
-                        let rep = await fetch('http://localhost:8000//books/api/v1.0/auteur');
-                        let reponse = await rep.json();
-                        return reponse;
-}
-
-
-function getCorrespondance(resultTranslategpt){
-let splitResultTranslategpt = resultTranslategpt.split(" ")
-    for(let i=0;i<splitResultTranslategpt.length;i++)
-    replace(splitResultTranslategpt[i], traduction-jeunes.json.resultTranslategpt[i].mots);
-
-})
-
-
-*/
+getJson();
