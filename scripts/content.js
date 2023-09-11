0d813ed1620894b02ced413b377df45a3c69899c
oscorchan <oscar.cortijos@icloud.com>
2023-09-11 12:01:41 +0200

    sauvegarde content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,47 @@
+function isExcluded(elm) {
+    if (elm.tagName == "STYLE") {
+      return true;
+    }
+    if (elm.tagName == "SCRIPT") {
+      return true;
+    }
+    if (elm.tagName == "NOSCRIPT") {
+      return true;
+    }
+    if (elm.tagName == "IFRAME") {
+      return true;
+    }
+    if (elm.tagName == "OBJECT") {
+      return true;
+    }
+    return false
+  }
+  
+  function traverse(elm) {
+    if (elm.nodeType == Node.ELEMENT_NODE || elm.nodeType == Node.DOCUMENT_NODE) {
+      // exclude elements with invisible text nodes
+      if (isExcluded(elm)) {
+        return
+      }
+      for (var i=0; i < elm.childNodes.length; i++) {
+        // recursively call to traverse
+        traverse(elm.childNodes[i]);
+      }
+    }
+  
+    if (elm.nodeType == Node.TEXT_NODE) {
+      // exclude text node consisting of only spaces
+      if (elm.nodeValue.trim() == "") {
+        return
+      }
+      // elm.nodeValue here is visible text we need.
+      console.log(elm.nodeValue);
+      elm.nodeValue = elm.nodeValue.replace("Google", "***");
+    }
+  }
+  
+  traverse(document);
+
 async function getJson() {
   try {
       const url = "http://127.0.0.1:5500/scripts/traduction-jeunes.json";
@@ -5,7 +49,9 @@ async function getJson() {
       const data = await response.json();
 
       const elements = document.querySelectorAll('h1,h2,h3,h4,h5,h6,p,li,td,caption');
-
+        //.foreach(el => {…})
+        //It iterates over each element in the elements. 
+        //For each element the function el is executed. This function checks if each element contains an <img> element and replacing specific word. 
       elements.forEach(el => {
           if (!el.querySelector('img')) {
               Object.keys(data).forEach(key => {
