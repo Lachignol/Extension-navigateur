e571cb1244442673d31c43df5a9a46f093d79dbb
Ghislaine Aybram <ghislaine.aybram@gmail.com>
2023-09-12 15:55:51 +0200

    bouton on off fonctionnant

--- scripts/background.js
+++ scripts/background.js
@@ -13,45 +13,39 @@ function stopAudioCode() {
     }
 }
 
-function recordBody(){
-    const body = document.body;
-    return body
-}
-
-function sendBody(recordBody){
-   return document.body = document
-   
-}
-
-
 async function getJson() {
     try {
         const url = "http://127.0.0.1:5500/scripts/traduction-jeunes.json";
         const response = await fetch(url);
         const data = await response.json();
-  
+
         const elements = document.querySelectorAll('h1,h2,h3,h4,h5,h6,p,li,td,caption');
-        
+
         elements.forEach(el => {
             if (!el.querySelector('img')) {
+            // on exclut les images pour ne pas casser les liens
                 Object.keys(data).forEach(key => {
-                  const regex = new RegExp(`(?<![="])\\b${key}\\b(?!["=])(?![^<]*>)`, "ig");
-                  const replacement = `<span style="color: red" title="${data[key].definition}">${data[key].mot}</span>`;
-                  el.innerHTML = el.innerHTML.replace(regex, replacement);
+                // The Object.keys() static method returns an array of a given object's own enumerable string-keyed property names.
+                    const regex = new RegExp(`(?<![="])\\b${key}\\b(?!["=])(?![^<]*>)`, "ig");
+                    // \b = début/fin de mot = word character is not followed or preceded by another word-character
+                    // i = case-insensitive (so, for example, /a/i will match the string "a" or "A".
+                    // g = global, match all instances of the pattern in a string, not just one
+                    // (?<![="]) = vérifie qu’il n’y a pas de caractère “=” ou “”" avant le début de la clé.
+                    // (?!["=]) = vérifie qu’il n’y a pas de caractère “=” ou “”" après la fin de la clé.
+                    // (?![^<]*>) = pour s’assurer qu’il n’y a pas de caractère “>” après la clé, ce qui indiquerait la fin d’une balise HTML <a>.
+                    const replacement = `<span style="color: red" title="${data[key].definition}">${data[key].mot}</span>`;
+                    el.innerHTML = el.innerHTML.replace(regex, replacement);
                 });
             }
         });
     } catch (err) {
         console.error("Error fetching or processing data:", err);
     }
-  }
-  
+}
 
-
-
-
-
-
+function resetContent() {
+    location.reload()
+}
 
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
@@ -68,37 +62,26 @@ chrome.action.onClicked.addListener(async (tab) => {
         text: nextState,
     });
 
-    chrome.tabs.sendMessage(tab.id, {action: nextState});
+    chrome.tabs.sendMessage(tab.id, { action: nextState });
 
     if (nextState === "ON") {
         await chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            function:playAudioCode,
-        })
-
-        await chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: recordBody
-            
-    
+            function: playAudioCode,
         });
-
         await chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: await getJson
-            
-                
-            
-
         });
+
     } else if (nextState === "OFF") {
         await chrome.scripting.executeScript({
             target: { tabId: tab.id },
             function: stopAudioCode
-
-        })
+        });
         await chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            function: window.location.href
-    })
-    }})
+            function: resetContent
+        });
+    }
+})
--- scripts/content.js
+++ scripts/content.js
@@ -1,42 +1,31 @@
-// console.log(originalContent)
 
-// function resetContent() {
-//   originalContent.forEach((originalHTML, element) => {
-//       element.innerHTML = originalHTML;
-//   });
+// async function getJson() {
+//   try {
+//     const url = "http://127.0.0.1:5500/scripts/traduction-jeunes.json";
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     const elements = document.querySelectorAll('h1,h2,h3,h4,h5,h6,p,li,td,caption');
+
+//     elements.forEach(el => {
+//       if (!el.querySelector('img')) {
+//         Object.keys(data).forEach(key => {
+//           const regex = new RegExp(`(?<![="])\\b${key}\\b(?!["=])(?![^<]*>)`, "ig");
+//           // \b = début/fin de mot = word character is not followed or preceded by another word-character
+//           // i = case-insensitive (so, for example, /a/i will match the string "a" or "A".
+//           // g = global, match all instances of the pattern in a string, not just one
+//           // (?<![="]) = vérifie qu’il n’y a pas de caractère “=” ou “”" avant le début de la clé.
+//           // (?!["=]) = vérifie qu’il n’y a pas de caractère “=” ou “”" après la fin de la clé.
+//           // (?![^<]*>) = pour s’assurer qu’il n’y a pas de caractère “>” après la clé, ce qui indiquerait la fin d’une balise HTML <a>.
+//           const replacement = `<span style="color: red" title="${data[key].definition}">${data[key].mot}</span>`;
+//           el.innerHTML = el.innerHTML.replace(regex, replacement);
+//         });
+//       }
+//     });
+//   } catch (err) {
+//     console.error("Error fetching or processing data:", err);
+//   }
 // }
-
-
-async function getJson() {
-  try {
-      const url = "http://127.0.0.1:5500/scripts/traduction-jeunes.json";
-      const response = await fetch(url);
-      const data = await response.json();
-
-      const elements = document.querySelectorAll('h1,h2,h3,h4,h5,h6,p,li,td,caption');
-      
-      elements.forEach(el => {
-          if (!el.querySelector('img')) {
-              Object.keys(data).forEach(key => {
-                const regex = new RegExp(`(?<![="])\\b${key}\\b(?!["=])(?![^<]*>)`, "ig");
-                const replacement = `<span style="color: red" title="${data[key].definition}">${data[key].mot}</span>`;
-                el.innerHTML = el.innerHTML.replace(regex, replacement);
-              });
-          }
-      });
-  } catch (err) {
-      console.error("Error fetching or processing data:", err);
-  }
-}
 
 // getJson()
 
-// // Écouteur pour les messages provenant du background.js
-// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-//     if (message.action === "ON") {
-//         getJson();  // Lance la fonction de traduction quand l'extension est sur ON
-//     }
-//     if (message.action === "OFF") {
-//         resetContent();
-//     }
-// });
