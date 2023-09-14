d8caf64c02b0467a7359c673e72edbf1552f27c9
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-09-14 11:08:55 +0200

    v-clean

--- scripts/key.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open ('traduction-jeunes.json', 'r') as f:
-    data = json.load(f)
-
-with open ('listeMots.txt', 'w') as text:
-    for cle in data.keys():
-        text.write(f"{cle} ")
